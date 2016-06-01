39e40548fa107deb43a449896d50d71117c6ea25
Kincses Dani <danikincs@gmail.com>
2016-06-01 13:11:00 +0200

    highscore added again, mulitplayer add

--- SnakeGame.py
+++ SnakeGame.py
@@ -8,6 +8,21 @@ from random import randint
 curses.initscr()
 
 
+def add():                                    # Add .txt file if not exists, stores player score.
+    input_file = open("name.txt", "a")
+    name = input("enter your username: ")
+    print(name, " ", score, file=input_file)
+    input_file.close()
+
+
+def print_score():                           # S hows score in terminal, previous scores also appears
+    output_file = open("name.txt", "r+")
+    text_in_file = output_file.read()
+    print(text_in_file)
+    output_file.close()
+    exit()
+
+
 def start_screen(scr):      # Start menu where you can choose single, or multiplayer game mode.
     win = curses.newwin(curses.LINES, curses.COLS, 0, 0)
     win.clear()
@@ -38,6 +53,7 @@ def end_scene(scr):                          # It appears, when player loses, re
         start_screen(scr)
     if event == ord("q"):
         curses.endwin()
+        add()
         print_score()
     time.sleep(1)
 
@@ -110,6 +126,115 @@ def single_player(scr):
 
     curses.endwin()
 
+def multi_player(scr):
+    win = curses.newwin(curses.LINES, curses.COLS, 0, 0)
+    min_max = [curses.LINES, curses.COLS]
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
+    game_over = False
 
+    key = KEY_RIGHT
+    key2 = ord("d")
+    global score_1                                                  # Initializing values
+    score_1 = 0
+    global score_2
+    score_2 = 0
+
+    snake_1 = [[4, 10], [4, 9], [4, 8]]                                     # Initial snake co-ordinates
+    snake_2 = [[6, 14], [6, 13], [6, 12]]
+    food = [10, 20]                                                    # First food co-ordinates
+
+    win.addch(food[0], food[1], '*')                                   # Prints the food
+
+    while key != 27 or key2 != 27:                             # While Esc key is not pressed
+        win.border(0)
+        win.addstr(0, 2, 'Score : ' + str(score_1) + ' ')                # Printing 'Score' and
+        win.addstr(0, 66, 'Score_2 : ' + str(score_2) + ' ')
+        win.addstr(0, 35, ' SNAKE ')                                   # 'SNAKE' strings
+        win.timeout(30)
+
+        prevKey = key                                                  # Previous key pressed
+        event = win.getch()
+        key = key if event == -1 else event
+
+        if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
+            key = -1                                                   # one (Pause/Resume)
+            while key != ord(' '):
+                key = win.getch()
+            key = prevKey
+            continue
+
+        prevKey2 = key2                                                  # Previous key pressed
+        event2 = win.getch()
+        key2 = key2 if event2 == -1 else event2
+
+        if key2 == ord(' '):                                            # If SPACE BAR is pressed, wait for another
+            key2 = -1                                                   # one (Pause/Resume)
+            while key2 != ord(' '):
+                key2 = win.getch()
+            key2 = prevKey
+            continue
+
+        if key2 not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27, "w", "a", "s", "d"]:     # If an invalid key is pressed
+            key2 = prevKey2
+
+        snake_1.insert(0, [snake_1[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake_1[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+        snake_2.insert(0, [snake_2[0][0] + (key == ord("s") and 1) + (key == ord("w") and -1), snake_2[0][1] + (key == ord("a") and -1) + (key == ord("d") and 1)])
+
+        if snake_1[0][0] == 0:  # goes up
+            snake_1[0][0] = 22
+        if snake_1[0][1] == 0:  # goes left
+            snake_1[0][1] = min_max[0]+54
+        if snake_1[0][0] == min_max[0]:
+            snake_1[0][0] = 1
+        if snake_1[0][1] == min_max[1]:
+            snake_1[0][1] = 1
+
+        if snake_2[0][0] == 0:  # goes up
+            snake_2[0][0] = 22
+        if snake_2[0][1] == 0:  # goes left
+            snake_2[0][1] = min_max[0]+54
+        if snake_2[0][0] == min_max[0]:
+            snake_2[0][0] = 1
+        if snake_2[0][1] == min_max[1]:
+            snake_2[0][1] = 1
+
+        # if snake_1[0] in snake_1[1:]:
+        #     end_scene(scr)
+        # if snake_2[0] in snake_2[1:]:
+        #     end_scene(scr)
+
+        if snake_1[0] == food:                                            # When snake eats the food
+            food = []
+            score_1 += 1
+            while food == []:
+                food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
+                if food in snake_1:
+                    food = []
+            win.addch(food[0], food[1], '*')
+        else:
+            last = snake_1.pop()                                          # [1] If it does not eat the food, length decreases
+            win.addch(last[0], last[1], ' ')
+        win.addch(snake_1[0][0], snake_1[0][1], '#')
+
+        if snake_2[0] == food:                                            # When snake eats the food
+            food = []
+            score_2 += 1
+            while food == []:
+                food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
+                if food in snake_2:
+                    food = []
+            win.addch(food[0], food[1], '*')
+        else:
+            last = snake_2.pop()                                          # [1] If it does not eat the food, length decreases
+            win.addch(last[0], last[1], ' ')
+        win.addch(snake_2[0][0], snake_2[0][1], '+')
+        win.refresh()
+
+    # curses.endwin()
+    end_scene(scr)
 
 curses.wrapper(start_screen)
